a44fbd0c1689e1fcae1e022f24fb1603b48cff0d
likedanana <65353140+likedanana@users.noreply.github.com>
2020-07-30 11:10:48 +0900

    Add files via upload

--- shopping-mall/script.js
+++ shopping-mall/script.js
@@ -1,3 +1,56 @@
-'use strict'
+'use strict';
 
-const imgs = ['blue_p', 'blue_s', 'blue_t', 'pink_p', 'pink_s', 'pink_t', 'yellow_p', 'yellow_s', 'yellow_t'];
+class Cloth{
+  constructor(type, color, jender, size){
+    this.type = type;
+    this.color = color;
+    this.jender = jender;
+    this.size = size;
+  }
+  
+  getLi(){
+    let itemLi = document.createElement('li');
+    itemLi.innerHTML = 
+    `<img src="imgs/${this.color}_${this.type[0]}.png" alt="${this.color} ${this.type}">${this.jender}, ${this.size} size`;
+    return itemLi;  
+  }
+}
+
+function addClothesToUl( ul, clothArr ){
+  clothArr.forEach(cloth => ul.append( cloth.getLi() ));
+}
+
+let clothes = [];
+clothes.push(new Cloth('tshirt', 'pink', 'female', 'small'));
+clothes.push(new Cloth('skirt', 'blue', 'male', 'large'));
+clothes.push(new Cloth('tshirt', 'yellow', 'female', 'large'));
+clothes.push(new Cloth('pants', 'yellow', 'female', 'small'));
+clothes.push(new Cloth('pants', 'blue', 'male', 'large'));
+clothes.push(new Cloth('skirt', 'pink', 'female', 'small'));
+clothes.push(new Cloth('tshirt', 'blue', 'male', 'small'));
+clothes.push(new Cloth('skirt', 'yellow', 'male', 'small'));
+clothes.push(new Cloth('tshirt', 'pink', 'female', 'large'));
+clothes.push(new Cloth('pants', 'blue', 'male', 'small'));
+clothes.push(new Cloth('pants', 'pink', 'male', 'large'));
+clothes.push(new Cloth('skirt', 'yellow', 'female', 'large'));
+clothes.push(new Cloth('skirt', 'pink', 'female', 'small'));
+clothes.push(new Cloth('tshirt', 'blue', 'male', 'large'));
+clothes.push(new Cloth('skirt', 'yellow', 'male', 'small'));
+clothes.push(new Cloth('tshirt', 'pink', 'female', 'large'));
+clothes.push(new Cloth('pants', 'blue', 'male', 'small'));
+clothes.push(new Cloth('pants', 'pink', 'male', 'large'));
+clothes.push(new Cloth('skirt', 'yellow', 'female', 'small'));
+
+const itemsUl = document.querySelector(".items");
+addClothesToUl( itemsUl, clothes );
+
+const typesUl = document.querySelector(".category");
+typesUl.onclick = function(event) {
+  itemsUl.innerHTML="";
+
+  let key = event.target.dataset.key;
+  let clicked = event.target.dataset.clicked;
+
+  let filteredClothes = clothes.filter( cloth => cloth[key] == clicked );
+  addClothesToUl( itemsUl, filteredClothes );
+}
